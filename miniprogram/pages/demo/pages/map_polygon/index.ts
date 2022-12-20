2f179c9514a19d6adc7235a34da9161bdc3348d8
wade <wadechen@outlook.com>
2022-12-20 09:49:54 +0800

    update

---
+++ miniprogram/pages/demo/pages/map_polygon/index.ts
@@ -0,0 +1,83 @@
+Page({
+  data: {
+    latitude: 23.099994,
+    longitude: 113.324520,
+    mapCtx: undefined as any,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: 'T.I.T 创意园'
+    }],
+    polygons: [
+      {
+        id: 2,
+        name: 'TEST',
+        fillColor: '#AAAAAACC',
+        points: [
+          {
+            latitude: 23.199994,
+            longitude: 113.424520,
+          },
+          {
+            latitude: 23.099994,
+            longitude: 113.324520,
+          },
+          {
+            latitude: 23.199994,
+            longitude: 113.324520,
+          }
+        ],
+      }
+    ],
+    covers: [{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '/image/location.png'
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '/image/location.png'
+    }]
+  },
+  onReady: function () {
+    this.data.mapCtx = wx.createMapContext('myMap')
+  },
+  getCenterLocation: function () {
+    this.data.mapCtx.getCenterLocation({
+      success: function(res: any){
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+  moveToLocation: function () {
+    this.data.mapCtx.moveToLocation()
+  },
+  translateMarker: function() {
+    this.data.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude:23.10229,
+        longitude:113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function() {
+    this.data.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude:23.10229,
+        longitude:113.3345211,
+      }, {
+        latitude:23.00229,
+        longitude:113.3345211,
+      }]
+    })
+  }
+})
